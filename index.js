c1236929cfa9f8692ccc2825ff28be4b7d3dbf7c
romansalom <roman.salom12@gmail.com>
2022-12-12 17:54:33 -0300

    id no funca

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const array = []
 
 class ProductManager{
-
-     constructor(title , descriptions , price , thumbnail , code , stock){
+    static id = 1;
+    
+     constructor(title , descriptions , price , thumbnail , code , stock , id){
         this.title = title,
         this.descriptions= descriptions,
          this.price = price,
         this.thumbnail = thumbnail ,
         this.code =code ,
-        this.stock = stock
+        this.stock = stock,
+        ProductManager.id;
      }
-    
-      
+   
     
 addProduct(){
-    array.push({
+
+    const productos = {
+       
         title : this.title,
         descriptions: this.descriptions,
         price : this.price ,
         thumbnail : this.thumbnail,
         code :  this.code,
-        stock : this.stock
-    })
-}}
+        stock : this.stock,
+        id : ProductManager.id,
+        
+        }
+       
+       
+    ProductManager.id + 1
+        array.push({productos});
+        
+    }
+    
+}
 
 
-let producto1 = new ProductManager("regla" , "30 cm" , 50 , "www.regla.com" , 4543 , 23);
+let producto1 = new ProductManager("regla" , "30 cm" , 50 , "www.regla.com" , 1323 , 23 );
+
+
 producto1.addProduct();
 let producto2 = new ProductManager("escuadra" , "triangular" , 12, "www.escuadra.com" , 1323 , 12);
 producto2.addProduct();
     
 
-console.log(array)
+console.log(array);
