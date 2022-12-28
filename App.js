028688f57516dc305a2d5a32792727ebb34aabf3
romansalom <roman.salom12@gmail.com>
2022-12-28 17:04:17 -0300

    no anda id

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const fs = require('fs');
 const {ProductManager}= require('./productManager');
@@ -8,21 +9,29 @@ const port = 8080;
 app.get('/products', (req,res)=>{
 
     const obtenerproductos = JSON.parse(fs.readFileSync('productos.json', 'utf-8'));
-    const title = req.query.title;
+    const limit = req.query.limit;
+    if(limit) return res.send(obtenerproductos.slice(0,limit))
 
-    res.send(obtenerproductos);
+    else return res.send(obtenerproductos)
+
+
 });
+
+
 app.get('/products/:title', (req,res)=>{
     const title = req.query.title;
     const obtenerproductos = JSON.parse(fs.readFileSync('productos.json' ,'utf-8'));
 
     res.send(obtenerproductos.find(e => e.title === req.params.title));
 });
+
+
+
 app.get('/products/:id', (req,res)=>{
     const id = req.query.id();
     const obtenerproductos = JSON.parse(fs.readFileSync('productos.json' ,'utf-8'));
 
-    res.send(obtenerproductos[id-1]);
+    res.send(obtenerproductos.find(e=>e.id === req.params.id))
 });
 app.listen(port, ()=>{
     console.log('servidor levantado en el purto' , port );
